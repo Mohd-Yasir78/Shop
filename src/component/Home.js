c73d258fc8c731464c63973d59aba75bd9941c2a
yasir <zoro68398@gmail.com>
2024-04-25 01:22:36 -0400

    update home

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import './Home.css'
+import image1 from './Photo/image1.jpg'
+import image2 from './Photo/child-hands-holding-caring-young-green-plant.jpg'
+import image3 from './Photo/close-up-picture-hand-holding-planting-seed-plant.jpg'
+import image4 from './Photo/farmer-spraying-vegetables-garden-with-herbicides-man-black-apron.jpg'
+
 
 const Home = () => {
   return (
@@ -12,10 +17,30 @@ const Home = () => {
       <div className='box1'>
           <div className='head'>Agricultural Seeds, Fertilizer and Pesticide</div>
             <div className='parent2'>
-              <div className='child1'></div>
-              <div className='child1'></div>
-              <div className='child1'></div>
-              <div className='child1'></div>
+              <div className='child'>
+               <div className='image'>
+                <img src={image1} alt="Loding"/>
+               </div>
+               <p>Fertilizer</p>
+              </div>
+              <div className='child'>
+              <div className='image'>
+                <img src={image2} alt="Loding"/>
+               </div>
+               <p>Soil</p>
+              </div>
+              <div className='child'>
+              <div className='image'>
+                <img src={image3} alt="Loding"/>
+               </div>
+               <p>Seed's</p>
+              </div>
+              <div className='child'>
+              <div className='image'>
+                <img src={image4} alt="Loding"/>
+               </div>
+               <p>Pesticide</p>
+              </div>
             </div>
           </div>
       </div>
